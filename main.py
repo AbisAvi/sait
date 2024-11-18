5cfe05fe50ee78e31a99d17afce189ca2bf68652
Python#4 <daniilvolodin4@gmail.com>
2024-11-18 18:57:20 +0400

    удолил закон всемирного теготения

--- main.py
+++ main.py
@@ -1,45 +1,11 @@
 from dataclasses import asdict
-from random import randint
-
-from flask import Flask, render_template, redirect
-
+from flask import Flask, render_template
 from forms.SignUpForm import SignUpForm
-from forms.form import GravitationForm
-from models import *
 
 users = []
 
 
-def generate_users(n: int):
-    for i in range(n):
-        user = User(i, "test@example.com")
-        users.append(user)
-
-
-def addUser(name):
-    if len(users) == 0:
-        last_id = 0
-    else:
-        last_id = max(users, key=lambda x: x.id).id + 1
-
-    user = User(last_id, name)
-    users.append(user)
-
-
 app = Flask(__name__)
-
-
-@app.route("/gr_wtf", methods=['GET', 'POST'])
-def gr_wtf():
-    form = GravitationForm()
-    if form.validate_on_submit():
-        m1 = form.m1.data
-        m2 = form.m2.data
-        r = form.r.data
-
-        f = (6 * m1 * m2) / (r ** 2)
-        return render_template("gravitation_wtf.html", form=form, output=f)
-    return render_template("gravitation_wtf.html", form=form)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -104,7 +70,6 @@ def delUser(user_id: int):
 
 
 if __name__ == '__main__':
-    generate_users(10)
     app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
     app.run(debug=True, port = 1029)
 
